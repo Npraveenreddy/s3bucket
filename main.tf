266acd27914c614871c5354642460df733933aca
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-13 09:27:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region     = "us-east-2"
+  access_key = "AKIATIB5K2VXSX3F3TO5"
+  secret_key = "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"
   }
   resource "aws_s3_bucket" "bucket1" {
     bucket = "delete-1"

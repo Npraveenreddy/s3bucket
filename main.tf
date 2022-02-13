8313fb3db8a724b397b7c507a5715860b2a503af
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-13 10:26:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,8 @@
-provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIATIB5K2VXSX3F3TO5"
-  secret_key = "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"
+terraform {
+  backend "s3" {
+    bucket  = "delete-1"
+    key     = "rds-mysql-state/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
-  resource "aws_s3_bucket" "bucket1" {
-    bucket = "delete-1"
-    
-    tags = {
-      Environment = "Dev"
-      }
-      }
-
+}

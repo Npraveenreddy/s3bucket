a6e265f1756d8ba1efdb6026c38644959d57ea5f
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-14 11:25:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "delete-1"
-    key     = "rds-mysql-state/"
+    key     = "mykey/terraform.tfstate"
     region  = "us-east-2"
     encrypt = true
   }

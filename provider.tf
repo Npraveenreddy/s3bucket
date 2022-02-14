09fb2d7684298e100c3b4b99d11ab8512ee5fb8c
Praveen Reddy Narayana <pnarayana@innominds.com>
2022-02-14 11:22:54 +0530

    added

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Declaring an AWS provider named aws
+provider "aws" {
+  # Declaring the provider region
+  region = "us-east-2"
+  # Declaring the access_key and secret_key
+  access_key = "AKIATIB5K2VXSX3F3TO5"
+  secret_key = "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"
+}

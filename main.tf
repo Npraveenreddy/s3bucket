d5d3b66b5815881cad92da1b439ef6407c1eabac
PraveenReddy I <narayana.praveen143@gmail.com>
2022-02-15 12:25:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@ terraform {
   backend "s3" {
     bucket = "delete-1"
     region = "us-east-2"
-    key = "yourkey/terraform.tfstate"
-    dynamodb_table = "your-lock-table"
-    encrypt = true
+    key = "praveen/terraform.tfstate"
     access_key = "AKIATIB5K2VXSX3F3TO5"
     secret_key = "U1PWDcCZKjcOTgVsu9kprD1jqmKsx9psuzxUWygn"
   }

c9f5dbdb4c18a473a8fae1106145bb9e87877b68
PraveenReddy I <narayana.praveen143@gmail.com>
2022-03-07 15:05:49 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "delete-1"
-    region = "us-east-2"
-    key = "rds-mysql-state/terraform.tfstate"
-    access_key = "AKIA4HDR3GBZJQKZIOM5"
-    secret_key = "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"
-  }
-}
